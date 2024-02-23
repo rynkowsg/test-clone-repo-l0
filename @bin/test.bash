7a87a29da1f77e9f5303997dade9f5625fefefd1
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2024-02-23 00:56:56 -0300

    Add repo-l1 (master)

--- @bin/test.bash
+++ @bin/test.bash
@@ -3,9 +3,10 @@
 ###
 # Test script consolidating tests in this repo and it's submodules.
 #
-# Example call:
+# Example calls:
 #
-#     ./@bin/test.bash
+#     SUBMODULES_ENABLED=0 ./@bin/test.bash
+#     SUBMODULES_ENABLED=1 ./@bin/test.bash
 #
 ###
 
@@ -17,5 +18,12 @@ SCRIPT_DIR="$(cd "$(dirname "${SCRIPT_PATH}")" || exit 1; pwd -P)"
 REPO_DIR="$(cd "${SCRIPT_DIR}/.." || exit 1; pwd -P)"
 # detect REPO_DIR - end
 
+# SUBMODULES_ENABLED - submodules support, if not specified, set to false
+SUBMODULES_ENABLED=${SUBMODULES_ENABLED:-0}
+
 echo "Running: ${REPO_DIR}/test/test_content.bats..."
 "${REPO_DIR}/test/test_content.bats"
+
+if [ "${SUBMODULES_ENABLED}" = 1 ]; then
+  "${REPO_DIR}/refs/repo-l1/@bin/test.bash"
+fi
